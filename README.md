bfe0b815aa4cb5dea6d235f06ce9c63dbccf8bc0
kiko-g <kikojpgoncalves@gmail.com>
2021-10-01 00:22:34 +0100

    optimize functionality and add results.json

---
+++ README.md
@@ -0,0 +1,3 @@
+# Finishers Web Scraper
+
+A project for learning web scraping and managing warzone finishers  
--- index.js
+++ index.js
@@ -1,52 +1,47 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
+const axios = require("axios")
+const cheerio = require("cheerio")
 const results = []
 
-function grabFinishers(index) {
-    //const storeURL = 'https://my.callofduty.com/player/store/sku/400039/title/mw'
-    const finishersURL = 'https://cod.tracker.gg/modern-warfare/db/loot?c=9&page=' + index.toString()
-    axios(finishersURL).then(function (response) {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const found = $('table tbody tr', html)
+function grabFinishers(index, verbose = false) {
+  //const storeURL = 'https://my.callofduty.com/player/store/sku/400039/title/mw'
+  const finishersURL = "https://cod.tracker.gg/modern-warfare/db/loot?c=9&page=" + index
+  axios(finishersURL).then(
+    function (response) {
+      const html = response.data
+      const $ = cheerio.load(html)
+      const found = $("table tbody tr", html)
 
-        if (found.length === 0) {
-            console.log(results.length + ' finishers found')
-        } else {
-            found.each(function () {
-                const title = $(this).find('.item-details__name').text()
-                results.push({
-                    id: results.length,
-                    title: title,
-                })
-            })
-            grabFinishers(index + 1)
+      if (found.length === 0) {
+        if (verbose) {
+          console.log(results)
+          console.log("No listings after page " + (index - 1).toString())
         }
-    }).catch(e => console.error(e))
+        console.log("Found " + results.length + " finishers")
+        dumpResults(results, "results.json")
+      } else {
+        found.each(function () {
+          const title = $(this).find(".item-details__name").text()
+          results.push({
+            id: results.length,
+            title: title,
+          })
+        })
+        if (verbose) console.log("Page " + index + " done")
+        grabFinishers(index + 1, verbose)
+      }
+    })
+    .catch((e) => console.error(e))
 }
 
-function grabFinishersMaxPage(maxIndex) {
-    for(let index = 1; index <= maxIndex; index++) {
-        let url = 'https://cod.tracker.gg/modern-warfare/db/loot?c=9&page=' + index
-        axios(url).then(function (response) {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const found = $('table tbody tr', html)
-
-            if (found.length === 0) {
-                console.log(results)
-                console.log(results.length + ' finishers found')
-            } else {
-                found.each(function () {
-                    const title = $(this).find('.item-details__name').text()
-                    results.push({
-                        id: results.length,
-                        title: title,
-                    })
-                })
-            }
-        }).catch(e => console.error(e))
+function dumpResults(jsonObject, filename) {
+  let fs = require("fs")
+  fs.writeFile(
+    "./" + filename,
+    JSON.stringify(jsonObject, null, 3),
+    function (error) {
+      return
     }
+  )
 }
 
-grabFinishersMaxPage(12)
+grabFinishers(1, true)
